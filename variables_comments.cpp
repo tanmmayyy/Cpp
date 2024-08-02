d154f12dc46ee59ea3833cf6d656871fd732861f
tanmmayyy <jaintanmay543@gmail.com>
2024-08-02 23:43:58 +0530

    variables and comments

---
+++ variables_comments.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+//this program was created by tanmay on 2 august 2024
+
+/*
+this 
+is 
+a
+multiple
+line
+comment
+*/
+
+int main(){
+    int sum = 6;
+    cout<<"hello world"<< sum;
+    return 0;
+}
