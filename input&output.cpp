7394efb184a7b09eef66175bd31bfe31f8ebe85b
tanmmayyy <jaintanmay543@gmail.com>
2024-08-06 00:27:02 +0530

    something

--- input&output.cpp
+++ input&output.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int num1, num2;
     cout<<"Enter the value of num1/n";                        // '<<' is called insertion operator
-    cin>>num1;                                              // '>>' is called extraction operator
+    cin>>num1;                                                // '>>' is called extraction operator
     cout<<"Enter the value of num2/n";
     cin>>num2;
     cout<<"The sum is "<< num1+num2;
