9fe6abe8f27e7893ee55a64f27bdfc62f518af09
tanmmayyy <jaintanmay543@gmail.com>
2024-08-05 23:41:38 +0530

    some changes

---
+++ dataType.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int a = 14, b = 15;
+    float pi = 3.14;
+    char c = 'a';
+
+
+    cout << "value of a is " << a << ". The value of b is " << b;
+    cout << "\nThe value of pi is " << pi;
+    cout << "\nThe value of c is " << c;
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int a = 14, b = 15;
+    float pi = 3.14;
+    cout << "value of a is " << a << ". The value of b is " << b;
+    cout << "\nThe value of pi is " << pi << endl;
+    return 0;
+}
