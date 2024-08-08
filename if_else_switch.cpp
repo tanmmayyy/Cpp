5a87981d80e17ace40a1defdac29f92e48f02f68
tanmmayyy <jaintanmay543@gmail.com>
2024-08-08 13:44:46 +0530

    added switch statements

--- if_else_switch.cpp
+++ if_else_switch.cpp
@@ -1,17 +1,43 @@
 #include<iostream>
 using namespace std;
 
+//selection control structure if  else-if else ladder
+
+
 int main(){
     int age;
     cout << "tell me your age";
     cin>>age;
-    if (age < 18){
-        cout << "you are not eligible";
-    }else if(age==18){
-        cout<<"u r idiot";
+    // if (age < 18){
+    //     cout << "you are not eligible";
+    // }else if(age==18){
+    //     cout<<"u r idiot";
+    // }
+    // else{
+    //     cout<<"u r eligible";
+    // }
+    // return 0;
+
+    //selection control structure: switch statements
+    switch (age)
+    {
+    case 18:
+        /* code */
+        cout << " you are 18";
+        break;
+    case 22:
+        /* code */
+        cout << " you are 22";
+        break;
+    case 5:
+        /* code */
+        cout << " you are 5";
+        break;
+    default:
+        cout << "No special cases"<<endl;
+        break;
     }
-    else{
-        cout<<"u r eligible";
-    }
-    return 0;
 }
+
+
+
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,3 @@
-#include<iostream>
-using namespace std;
- 
-int main(){
-    int a = 14, b = 15;
-    float pi = 3.14;
-    cout << "value of a is " << a << ". The value of b is " << b;
-    cout << "\nThe value of pi is " << pi << endl;
-    return 0;
-}
+    int age;
+    cout << "tell me your age";
+    cin>>age;
