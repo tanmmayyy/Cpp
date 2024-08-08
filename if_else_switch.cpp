8088756e7c0a20fa04d9f311b2f74ef6b7a6734a
tanmmayyy <jaintanmay543@gmail.com>
2024-08-08 13:27:24 +0530

    learning if else/ switch statements

---
+++ if_else_switch.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "tell me your age";
+    cin>>age;
+    if (age < 18){
+        cout << "you are not eligible";
+    }else if(age==18){
+        cout<<"u r idiot";
+    }
+    else{
+        cout<<"u r eligible";
+    }
+    return 0;
+
+}
