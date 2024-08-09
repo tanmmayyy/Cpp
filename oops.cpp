dd1125512caddc1830fae3d70c5047c893698f96
tanmmayyy <jaintanmay543@gmail.com>
2024-08-09 16:55:23 +0530

    learning oops

---
+++ oops.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Employee{
+public: 
+    string Name;
+    string Company;
+    int Age;
+
+    void introduce(){                        //method
+        cout<< "Name- "<<Name<<endl;
+        cout<< "Company- "<<Company<<endl;
+        cout<< "Age- "<<Age<<endl;
+
+    }
+};
+
+
+int main(){
+    Employee employee1;
+    employee1.Name = "Notty Fufa";
+    employee1.Company = "Notty Adda";
+    employee1.Age = 84;
+    employee1.introduce()
+}
