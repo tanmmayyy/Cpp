06e98a178fd9c1a99311a0d6c8d13ee2ed57be3d
tanmmayyy <jaintanmay543@gmail.com>
2024-08-09 17:13:44 +0530

    constructors

--- oops.cpp
+++ oops.cpp
@@ -13,13 +13,26 @@ public:
         cout<< "Age- "<<Age<<endl;
 
     }
+
+    Employee(string name, string company, int age){
+        Name = name;
+        Company = company;
+        Age = age;
+
+    }
+
 };
 
 
 int main(){
-    Employee employee1;
-    employee1.Name = "Notty Fufa";
-    employee1.Company = "Notty Adda";
-    employee1.Age = 84;
+    // Employee employee1;
+    // employee1.Name = "Notty Fufa";
+    // employee1.Company = "Notty Adda";
+    // employee1.Age = 84;
+
+    Employee employee1 = Employee("Notty Fufa","Notty Adda", 84);
     employee1.introduce();
+
+    Employee employee2 = Employee("John","WWE", 45);
+    employee2.introduce();
 }
