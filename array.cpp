944e23f69beb423132ba3ee73f43a4c7a915e4dc
tanmmayyy <jaintanmay543@gmail.com>
2024-08-15 23:26:21 +0530

    input in array

--- array.cpp
+++ array.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 using namespace std;
 
-// declaration of array
+
 
 int main(){
+
+    // declaration of array
+    int ele;
     int n;
     int arr[n];
 
     cout << "Enter the size of array ";
     cin>>n;
+
+    
+    
+    // taking input of elements
+
+    for(int i =0; i<n; i++){
+        cout << "Enter the element: ";
+        cin>>ele;
+        arr[i]=ele;
+    }
+
     return 0;
 }
