967c2cca2983cb9c87c0aa7314115e431ac68b26
tanmmayyy <jaintanmay543@gmail.com>
2024-08-15 23:21:33 +0530

    learning array

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// declaration of array
+
+int main(){
+    int n;
+    int arr[n];
+
+    cout << "Enter the size of array ";
+    cin>>n;
+    return 0;
+}
