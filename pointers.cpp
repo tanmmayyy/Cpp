46e78ddc4478d69d0ca92387d8ec958da08e4243
tanmmayyy <jaintanmay543@gmail.com>
2024-08-29 21:34:24 +0530

    learning basics of pointers

---
+++ pointers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // what is a pointer --->  data type which holds the adderess of other datatype
+
+    int a = 3;
+    int *b = &a;
+
+    // & ---> (address of ) operator
+    cout << "Address of a is: "<< &a <<endl;
+    cout << "Address of a is: "<< b <<endl;
+    
+    
+    
+    // * ---> dereference operator
+
+    cout << "The value at address b is: "<< *b <<endl;
+
+
+}
