3d2768212df4f172ff43f103bcba752c8d785241
tanmmayyy <jaintanmay543@gmail.com>
2024-09-11 23:09:18 +0530

    function of return type void

---
+++ functions.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void helloWorld(){
+    cout << "hello world";
+
+}
+int sum(int a, int b){
+    return a+b;
+}
+
+int main(){
+    helloWorld();
+    cout << endl;
+    cout << sum(2,3);
+}
+
+
+
+
+
+
